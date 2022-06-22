d30b3d3ed5ae8036f0b96fb13b8a7771cc3d7f90
nick100lay <nick100lay@gmail.com>
2022-06-22 16:41:38 +0300

    Fix static files

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -132,6 +132,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "news/static")
+]
+
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
